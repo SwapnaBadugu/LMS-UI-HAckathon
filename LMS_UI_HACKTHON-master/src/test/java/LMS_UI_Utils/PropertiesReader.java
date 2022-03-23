d4a6c10a70b12e618acf4a1a3c5321198880f91a
SwapnaBadugu <swapnathati78@gmail.com>
2022-03-23 18:33:51 -0400

    space added

--- LMS_UI_HACKTHON-master/src/test/java/LMS_UI_Utils/PropertiesReader.java
+++ LMS_UI_HACKTHON-master/src/test/java/LMS_UI_Utils/PropertiesReader.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertiesReader {
-	FileInputStream fis;
+	FileInputStream fis; 
 
 	public Properties loadProperties() {
 		try {
